17f83ea732b896a6e8f248eb23598b5f4d4f50b0
Matheus Silva <matheus-felipe97@hotmail.com>
2021-05-06 22:03:27 -0300

    Adicionando classes padrão geradas pelo Spring Boot

---
+++ src/main/java/per/desafiob2w/planetas/PlanetasApplication.java
@@ -0,0 +1,17 @@
+package per.desafiob2w.planetas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Classe principal do programa.
+ * 
+ * @author: Matheus Silva
+*/
+@SpringBootApplication
+public class PlanetasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/per/desafiob2w/planetas/PlanetasApplicationTests.java
@@ -0,0 +1,13 @@
+package per.desafiob2w.planetas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanetasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
